af15a4f26960f7a09ab4fedfc035c80bbe1a6c12
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-07-07 09:57:30 -0500

    feat: map

--- src/containers/Videos.js
+++ src/containers/Videos.js
@@ -1,40 +1,22 @@
 import React from 'react'
 import Video from '../components/VideoFuncion';
 // import Video from '../components/VideoClass';
+import {data} from '../utils/products';
 
 const Videos = () => {
   return (
     <>
-    <Video
-        title="Rod Stewart - Young Turks (Official Video)"
-        dateAdded="2009-10-29T21:26:05Z"
-        channel="Rod Stewart"
-        thumbnail="https://i.ytimg.com/vi/zQ41hqlV0Kk/mqdefault.jpg"
-        description="Official music video for Rod Stewart – Young Turks from 'Tonight I'm Yours' (1981) Listen to more Rod Stewart here: https://rhino.lnk.to/RodStewartStr ..." />
-      <Video
-        title="Rod Stewart - Baby Jane (Official Video)"
-        dateAdded="2014-03-01T01:35:21Z"
-        channel="Rod Stewart"
-        thumbnail="https://i.ytimg.com/vi/dxl2r6GuL2w/mqdefault.jpg"
-        description="Official music video for Rod Stewart – “Baby Jane” from 'Body Wishes' (1983) Listen to more Rod Stewart here: https://rhino.lnk.to/RodStewartStr Watch all ..." />
-      <Video
-        title="Bizcocho marmolado de naranja. SUPERESPONJOSO"
-        dateAdded="2019-09-25T18:16:56Z"
-        channel="Anna recetasfaciles"
-        thumbnail="https://i.ytimg.com/vi/3GwA45Bcvrs/mqdefault.jpg"
-        description="Compra mi libro de recetas COCINA PARA TRIUNFAR en este link https://amzn.to/2UPUhNZ Mi sartén Tefal Aroma 24 cm https://amzn.to/2WSFjLf Batidora de ..." />
-      <Video
-        title="Jarabe De palo - Ying Yang"
-        dateAdded="2008-12-04T14:37:33Z"
-        channel="Warner Music Spain"
-        thumbnail="https://i.ytimg.com/vi/JSH321S6tGc/mqdefault.jpg"
-        description="" />
-      <Video
-        title="La Niña: ¿Podrá Latinoamérica plantarle cara?"
-        dateAdded="2021-10-23T00:15:00Z"
-        channel="DW Español"
-        thumbnail="https://i.ytimg.com/vi/AsiUBtAQ4wU/mqdefault.jpg"
-        description="Duros inviernos, huracanes más intensos y grandes sequías. Son las consecuencias de La Niña, un fenómeno que se hará notar por segundo año consecutivo." />
+    {
+      data.map((item, index) => 
+        <Video
+        key={index}
+        title={item.title}
+        dateAdded={item.dateAdded}
+        channel={item.channel}
+        thumbnail={item.thumbnail}
+        description={item.description} />
+        )
+    }
     </>
   )
 }
---
+++ src/utils/products.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+      title: "Rod Stewart - Young Turks (Official Video)",
+      dateAdded: "2009-10-29T21:26:05Z",
+      channel: "Rod Stewart",
+      thumbnail: "https://i.ytimg.com/vi/zQ41hqlV0Kk/mqdefault.jpg",
+      description: "Official music video for Rod Stewart – Young Turks from 'Tonight I'm Yours' (1981) Listen to more Rod Stewart here: https://rhino.lnk.to/RodStewartStr ..."
+    },
+    {
+      title: "Rod Stewart - Baby Jane (Official Video)",
+      dateAdded: "2014-03-01T01:35:21Z",
+      channel: "Rod Stewart",
+      thumbnail: "https://i.ytimg.com/vi/dxl2r6GuL2w/mqdefault.jpg",
+      description: "Official music video for Rod Stewart – “Baby Jane” from 'Body Wishes' (1983) Listen to more Rod Stewart here: https://rhino.lnk.to/RodStewartStr Watch all ..."
+    },
+    {
+      title: "Bizcocho marmolado de naranja. SUPERESPONJOSO",
+      dateAdded: "2019-09-25T18:16:56Z",
+      channel: "Anna recetasfaciles",
+      thumbnail: "https://i.ytimg.com/vi/3GwA45Bcvrs/mqdefault.jpg",
+      description: "Compra mi libro de recetas COCINA PARA TRIUNFAR en este link https://amzn.to/2UPUhNZ Mi sartén Tefal Aroma 24 cm https://amzn.to/2WSFjLf Batidora de ..."
+    },
+    {
+      title: "Jarabe De palo - Ying Yang",
+      dateAdded: "2008-12-04T14:37:33Z",
+      channel: "Warner Music Spain",
+      thumbnail: "https://i.ytimg.com/vi/JSH321S6tGc/mqdefault.jpg",
+      description: ""
+    },
+    {
+      title: "La Niña: ¿Podrá Latinoamérica plantarle cara?",
+      dateAdded: "2021-10-23T00:15:00Z",
+      channel: "DW Español",
+      thumbnail: "https://i.ytimg.com/vi/AsiUBtAQ4wU/mqdefault.jpg",
+      description: "Duros inviernos, huracanes más intensos y grandes sequías. Son las consecuencias de La Niña, un fenómeno que se hará notar por segundo año consecutivo."
+    }
+  ];
